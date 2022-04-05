73e01118d3f62fd94d841a41a52e23c1ff78dbc9
quirinehoog <97471566+quirinehoog@users.noreply.github.com>
2022-04-05 10:13:58 -0700

    added axios running in app

---
+++ src/Weather.js
@@ -0,0 +1,12 @@
+import React from "react";
+const axios = require("axios").default;
+
+
+export default function Weather (){
+    return (
+        <div className="Weather">
+            <h2>Hello from React</h2>
+
+        </div>
+    );
+}
