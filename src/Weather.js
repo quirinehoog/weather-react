a01d3fb3c1961a3b70f115c3c0b39a0ff6cf8b6c
quirinehoog <97471566+quirinehoog@users.noreply.github.com>
2022-04-05 11:56:14 -0700

    add Weather.js to file

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,36 @@
 import React from "react";
 
-
-
-export default function Weather (){
-    return (
-        <div className="Weather">
-            <h2>Hello from React</h2>
-
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <div className="row">
+        <div className="col-6">
+          <div className="weather-leftside">
+            <h1>Paris</h1>
+            <h6 className="now">Thursday 12:00</h6>
+            <span className="temperature" id="temperature">
+              5 °C
+            </span>
+          </div>
         </div>
-    );
+        <div className="col-6">
+          <div className="weather-rightside">
+            <div className="weatherimage-rightside">
+              <img
+                src="http://openweathermap.org/img/wn/10d@2x.png"
+                alt="Clear"
+                width="110"
+              />
+            </div>
+            <div className="weatherinfo-rightside">
+              <ul>
+                <li>Humidity: 45 %</li>
+                <li>Wind: 2 km/h</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
