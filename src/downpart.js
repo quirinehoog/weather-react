8aa2637e7d845a2bd27961e22d4d37a2e53d7cfb
quirinehoog <97471566+quirinehoog@users.noreply.github.com>
2022-04-06 14:33:22 -0700

    footer downpart in line

--- src/downpart.js
+++ src/downpart.js
@@ -3,7 +3,15 @@ import React from "react";
 export default function Downpart (){
     return (
       <div className="Downpart">
-        < a href="https://github.com/quirinehoog/weather-react">Github</a>
+        <p>
+          This project is made by Quirine van 't Hoog and is open sourced on
+          <a
+            href="https://github.com/quirinehoog/weather-react"
+            target="_blank"
+          >
+            Github
+          </a>
+        </p>
       </div>
     );
 }
