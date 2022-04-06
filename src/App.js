17d615f27ec40b3a8f7f963cbe23c60447dd72ae
quirinehoog <97471566+quirinehoog@users.noreply.github.com>
2022-04-06 12:00:32 -0700

    change h1 to weather app

--- src/App.js
+++ src/App.js
@@ -3,23 +3,17 @@ import Header from "./Header";
 import Weather from "./Weather";
 import Forecast from "./Forecast";
 
-import './App.css';
+import "./App.css";
 import Downpart from "./downpart";
 
 function App() {
   return (
     <div className="App">
-      
-        
-        <h1>
-         Hallo World
-        </h1>
-        <Header />
-        <Weather />
-        <Forecast />
-        <Downpart />
-      
-      
+      <h1>Weather React</h1>
+      <Header />
+      <Weather />
+      <Forecast />
+      <Downpart />
     </div>
   );
 }
