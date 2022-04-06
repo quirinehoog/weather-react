828b30e1a84abeffac0eabd18dca1b4c4b2c5241
quirinehoog <97471566+quirinehoog@users.noreply.github.com>
2022-04-06 14:29:41 -0700

    removed unnecessary items

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import Header from "./Header";
 import Weather from "./Weather";
 import Forecast from "./Forecast";
+import Downpart from "./downpart";
 
 import "./App.css";
-import Downpart from "./downpart";
+
 
 function App() {
   return (
