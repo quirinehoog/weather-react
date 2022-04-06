7d197642610fd21de5711b314524609441a1e050
quirinehoog <97471566+quirinehoog@users.noreply.github.com>
2022-04-06 14:34:19 -0700

    update downpart

--- src/downpart.js
+++ src/downpart.js
@@ -1,17 +1,14 @@
 import React from "react";
 
-export default function Downpart (){
-    return (
-      <div className="Downpart">
-        <p>
-          This project is made by Quirine van 't Hoog and is open sourced on
-          <a
-            href="https://github.com/quirinehoog/weather-react"
-            target="_blank"
-          >
-            Github
-          </a>
-        </p>
-      </div>
-    );
+export default function Downpart() {
+  return (
+    <div className="Downpart">
+      <p>
+        This project is made by Quirine van 't Hoog and is open sourced on
+        <a href="https://github.com/quirinehoog/weather-react" target="_blank">
+          Github
+        </a>
+      </p>
+    </div>
+  );
 }
