166e745ecae1c90b5943182aa5505480455e5c2a
quirinehoog <97471566+quirinehoog@users.noreply.github.com>
2022-04-13 15:05:01 -0700

    dayforecast adding API to code

--- src/Dayforecast.js
+++ src/Dayforecast.js
@@ -2,23 +2,35 @@ import React from "react";
 import "./Dayforecast.css";
 
 export default function Dayforecast(props) {
-  let dayData = {
-    tempHigh: "11",
-    tempLow: "8",
-    day: "THU",
-    imgUrl: "http://openweathermap.org/img/wn/10d@2x.png",
-  };
+  function tempHigh(){
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}`;
+  }
 
-  return (
+   function tempLow() {
+     let temperature = Math.round(props.data.temp.min);
+     return `${temperature}`;
+   }
+  
+  function day() {
+    let date= new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+     return days[day];
+  }
+  
+    return (
     <div className="Dayforecast">
-      <img src={dayData.imgUrl} alt="weather" width="50" />
+      <WeatherIcon code={props.data.weather[0].icon} alt="weather" width="50" />
       <div className="weather-forecast-temps">
         <span className="weather-forecast-temps-high">
-          {dayData.tempHigh}°C{" "}
+          {tempHigh()}°C{" "}
         </span>
-        <span className="weather-forecast-temps-low">{dayData.tempLow}°C</span>
+        <span className="weather-forecast-temps-low">{tempLow()}°C</span>
       </div>
-      <div className="weather-forecast-day">{dayData.day}</div>
+      <div className="weather-forecast-day">{day()}</div>
     </div>
   );
 }
