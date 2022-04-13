dc1aaf08f958afe6695d4961269831ed38aae9df
quirinehoog <97471566+quirinehoog@users.noreply.github.com>
2022-04-13 16:07:04 -0700

    pushing updates in styling

--- src/Dayforecast.js
+++ src/Dayforecast.js
@@ -3,33 +3,29 @@ import WeatherIcon from "./WeatherIcon";
 import "./Dayforecast.css";
 
 export default function Dayforecast(props) {
-  function tempHigh(){
+  function tempHigh() {
     let temperature = Math.round(props.data.temp.max);
     return `${temperature}`;
   }
 
-   function tempLow() {
-     let temperature = Math.round(props.data.temp.min);
-     return `${temperature}`;
-   }
-  
-  function day() {
-    let date= new Date(props.data.dt * 1000);
-    let day = date.getDay();
-
-     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-     return days[day];
+  function tempLow() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}`;
   }
-  
-    return (
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+    let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    return days[day];
+  }
+
+  return (
     <div className="Dayforecast">
       <WeatherIcon code={props.data.weather[0].icon} alt="weather" width="50" />
       <div className="weather-forecast-temps">
-        <span className="weather-forecast-temps-high">
-          {tempHigh()}°C{" "}
-        </span>
-        <span className="weather-forecast-temps-low">{tempLow()}°C</span>
+        <span className="weather-forecast-temps-high">{tempHigh()} </span>
+        <span className="weather-forecast-temps-low">{tempLow()}</span>
       </div>
       <div className="weather-forecast-day">{day()}</div>
     </div>
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -3,42 +3,36 @@ import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
 
-export default function WeatherInfo(props){
-    return(
-<div className="WeatherInfo">
-  <div className="row">
-    <div className="col-6">
-      <div className="weather-leftside">
-        <ul>
-          <li>
-            <h1>{props.data.city}</h1>
-          </li>
-          <li>
-            <FormattedDate date={props.data.date}/>
-          </li>
-          <li>
-            <div className="weatherimage-rightside">
-              <WeatherIcon code={props.data.icon}
-                alt="Clear"
-                width="110"
-              />
-              <WeatherTemperature celsius={props.data.temperature} />
-              
-            </div>
-          </li>
-        </ul>
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <div className="row">
+        <div className="col-8">
+          <div className="weather-leftside">
+            <ul>
+              <li>
+                <h1>{props.data.city}</h1>
+              </li>
+              <li>
+                <FormattedDate date={props.data.date} />
+              </li>
+              <li>
+                <WeatherIcon code={props.data.icon} alt="Clear" />
+                <WeatherTemperature celsius={props.data.temperature} />
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="weatherinfo-rightside">
+            <ul>
+              <li>Precipitation: {props.data.precipitation}%</li>
+              <li>Humidity: {props.data.humidity} %</li>
+              <li>Wind: {props.data.wind} km/h</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
-    <div className="col-6">
-      <div className="weatherinfo-rightside">
-        <ul>
-          <li>Precipitation: {props.data.precipitation}%</li>
-          <li>Humidity: {props.data.humidity} %</li>
-          <li>Wind: {props.data.wind} km/h</li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</div>
-    );
+  );
 }
